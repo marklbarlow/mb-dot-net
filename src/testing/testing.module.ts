3c3241fe081b45bd74a99ad6e452dcb9b5658107
marklbarlow <Mark@Hibiki.connect>
2017-01-28 22:59:50 +0000

    AOT support

---
+++ src/testing/testing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import * as root from './';
+
+@NgModule({
+    declarations: [
+        root.MbGalleryStubComponent,
+        root.MdToolbarStubComponent,
+        root.RouterLinkStubDirective,
+        root.RouterOutletStubComponent,
+    ],
+})
+export class TestingModule { }
