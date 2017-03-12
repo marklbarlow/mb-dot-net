c2aa958205ba7d4226bc4434d69bc4104b91fd4d
marklbarlow <mark.l.barlow@gmail.com>
2017-03-12 15:57:01 +0000

    Fix failing test

--- src/app/components/app-nav-bar/app-nav-bar.component.spec.ts
+++ src/app/components/app-nav-bar/app-nav-bar.component.spec.ts
@@ -4,18 +4,28 @@ import 'hammerjs';
 
 import { RouterLinkStubDirective } from '../../../testing';
 import { AppNavBarComponent } from './app-nav-bar.component';
+import { AngularFireService } from '../../angularfire.service';
 
 describe('AppNavBarComponent', () => {
   let component: AppNavBarComponent;
   let fixture: ComponentFixture<AppNavBarComponent>;
+  let afServiceStub: any;
 
   beforeEach(async(() => {
+
+    afServiceStub = {
+      auth() { return undefined; }
+    };
+
     TestBed.configureTestingModule({
       declarations: [
         AppNavBarComponent,
         RouterLinkStubDirective,
       ],
-      imports: [MaterialModule.forRoot()]
+      imports: [MaterialModule.forRoot()],
+      providers: [
+        { provide: AngularFireService, useValue: afServiceStub },
+      ]
     })
       .compileComponents();
   }));
